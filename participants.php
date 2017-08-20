52ad98661b0c962814ca296992734d679dbaa05a
Emmanuel Joseph <jekayode@live.com>
2017-08-20 13:37:06 +0100

    Add Participants Page and initial Style

---
+++ participants.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+<table class="table user-list" width="80%">
+					<thead>
+						<tr>
+							<th><span>User</span></th>
+							<th><span>Team</span></th>
+							<th class="text-center"><span>Github</span></th>
+							<th><span>State 1 Output</span></th>
+							<th><span>Slack</span></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+								<a href="#" class="user-link">Emmanuel Joseph</a>
+								<span class="user-subhead">Email: jekayode@live.com</span>
+							</td>
+							<td>
+								Team 3
+							</td>
+							<td class="text-center">
+								<a href="https://github.com/jekayode" target="_blank">@jekayode</a>
+							</td>
+							<td>
+								<a href="https://github.com/jekayode/hnl-internship" target="_blank">#Stage1 Output</a>
+							</td>
+							<td style="width: 20%;">
+								<a href="https://hnginterns.slack.com">@jekayode</a>
+							</td>
+						</tr>
+
+						
+						
+					</tbody>
+				</table>
