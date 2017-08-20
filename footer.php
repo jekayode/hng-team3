1a2c85a64e6fb6d83a6d9e7eb9895e76c46ac900
Bosun Egberinde <bosunski@gmail.com>
2017-08-20 15:03:11 +0100

    Made Some Changes and combined with #team1 works.

---
+++ footer.php
@@ -0,0 +1,42 @@
+<!--Favicon-->
+<link rel="stylesheet" href="css/style.css">
+
+<link rel="stylesheet" href="css/color.css">
+
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<!-- Animate CSS-->
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+
+<!--Google Webfonts-->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+<!--Footer-->
+<footer id="landing-footer" class="clearfix">
+    <div class="row clearfix">
+
+        <p id="copyright" class="col-2">Made with love by Team 1</a></p>
+
+    </div>
+</footer>
+<!--End of Footer-->
+
+</div>
+
+<!-- Include JavaScript resources -->
+<script src="js/jquery.1.8.3.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/featherlight.min.js"></script>
+<script src="js/featherlight.gallery.min.js"></script>
+<script src="js/jquery.enllax.min.js"></script>
+<script src="js/jquery.scrollUp.min.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery.stickyNavbar.min.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/images-loaded.min.js"></script>
+<script src="js/lightbox.min.js"></script>
+<script src="js/site.js"></script>
+
+
+</body>
+</html>
